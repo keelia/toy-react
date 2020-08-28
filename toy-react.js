6ba3ea2eea9202cfd9d1b1779ac0eb8c8fbfa47a
likang <lkang@baseplate.co.nz>
2020-08-28 21:21:52 +1200

    re-render by Vdom comparison

--- toy-react.js
+++ toy-react.js
@@ -1,51 +1,4 @@
 const RENDER_TO_DOM = Symbol("render to dom")//使用symbo使它变成私有方法
-class ElementWrapper{//实体dom元素：得有root，可以挂载dom children或者挂载在别的实体dom上
-    constructor(type){
-        this.root = document.createElement(type)
-    }
-
-    setAttribute(name,value){
-        if(name.match(/^on([\S\s]+)$/)){//tips: [\s\S]+ 表示匹配所有字符
-            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/,c=>c.toLowerCase()),value)//支持驼峰表达的事件
-        }else{
-            if(name === 'className'){//react把classname直接换成了class
-                this.root.setAttribute('class',value)
-            }else{
-                this.root.setAttribute(name,value)
-            }
-            
-        }
-       
-    }
-    appendChild(component){
-        //以下需要改造，因为没有root可以用了，而是用的range([RENDER_TO_DOM](range))
-       // this.root.appendChild(component.root)//component的root要取出来，因为component是toyreact的component
-        //1.appenchild是把元素放在最后的，所以替换掉component的最后一个childnode
-        let range = document.createRange()
-        range.setStart(this.root,this.root.childNodes.length)
-        range.setEnd(this.root,this.root.childNodes.length)
-        range.deleteContents()
-        //2.把component的真实元素（root）放进去
-        component[RENDER_TO_DOM](range)
-    }
-    [RENDER_TO_DOM](range){
-        range.deleteContents()//删掉内容
-        range.insertNode(this.root)//重新插入
-    }
-}
-
-class TextWrapper{//实体dom text元素：得有root，可以被挂载在实体dom元素上。没有attr也没有child
-    constructor(content){
-        this.root = document.createTextNode(content)
-    }
-    [RENDER_TO_DOM](range){
-        range.deleteContents()//删掉内容
-        range.insertNode(this.root)//重新插入
-    }
-}
-
-//基于jsx语法的自定义组件，不是实体dom元素，而是由实体dom元素组成的虚拟dom元素类，必须有render方法，或者它的组成成分
-//在render函数返回的实体dom元素（可能包含别的虚拟dom元素类），
 export class Component{
     constructor(){
         this.props = Object.create(null)
@@ -61,42 +14,87 @@ export class Component{
         this.children.push(child)
     }
 
-    // //实现更新的话就没办法用root去更新了，虽然root是跟渲染相关的
-    // get root(){
-    //     if(!this._root){
-    //         this._root = this.render().root;//render出来的额仍然是个comoinent的话，这里就会发生递归，一直到component是一个text节点或者元素节点textWrapper/elementWrapper位置，可以返回root
-    //     }
-    //     return this._root
-    // }
+    get vdom(){
+        return this.render().vdom
+    }
 
     [RENDER_TO_DOM](range){//负责更新组件：需要给它一个参数，创建component之后，做了redner的时候，其实是需要知道具体的位置的，不是只知道element，因为不一定element都插入到最后
         //range api是有关位置的,取一个元素然后把它渲染进range里面
         //为支持重新绘制，需要对比变化前后，需要先把range存一下
         this._range = range
-        this.render()[RENDER_TO_DOM](range)
+        this._vdom = this.vdom//_vdom:old vdom, to be compared in update method
+        this._vdom[RENDER_TO_DOM](range)
     }
-    rerender(){
-        //把原来range里面的东西全删掉
-        //this._range.deleteContents()
-        //Range使用注意：这里删除了一个range里的全部内容，成为了一个全空的range，但是全空的range会被吞进下一个rangel里面，所以目前这个阶段如果从左往右点击格子时候，会出现最右边的格子线不见了的情况
-        //所以rerender的时候保证range是不空的，应该先插入再删除
-        let oldRange = this._range//保存旧的range
 
-        let range = document.createRange()
-        range.setStart(oldRange.startContainer,oldRange.startOffset)//新建的range放在老range的开始位置
-        range.setStart(oldRange.startContainer,oldRange.startOffset)
-        this[RENDER_TO_DOM](range)
-        
-        oldRange.setStart(range.endContainer,range.endOffset)//把老的range放在插入的range之后，然后删掉老的range
-        oldRange.deleteContents()
-        
+    update(){
+        let isSameNode = (oldNode,newNode)=>{
+            if(oldNode.type!==newNode.type){
+                return false
+            }
+
+            for (const name in newNode.props) {
+                if(newNode.props[name]!==oldNode.props[name]){
+                    return false
+                }
+            }
+            if(Object.keys(oldNode.props).length > Object.keys(newNode.props).length ){
+                return false
+            }
+            if(newNode.type ==='#text'){
+                if(newNode.content!==oldNode.content){
+                    return false
+                }
+            }
+            return true
+        }
+        let update = (oldNode,newNode)=>{
+            //compare root node: type(complete rerender),props(complete rerender)
+            //compare children
+            //#text: content
+            if(!isSameNode(oldNode,newNode)){
+                //override old node
+                newNode[RENDER_TO_DOM](oldNode._range)//replace oldnode's range
+                return
+            }
+            //set oldnode'range as newNode range
+            newNode._range = oldNode._range//only when oldnode/newnode is elemenwrapper
+
+            //children
+            let newVChildren = newNode.vchildren
+            let oldVChildren = oldNode.vchildren
+
+            if(!newVChildren ||!newVChildren.length){
+                return
+            }
+
+            let tailRange = oldVChildren[oldVChildren.length -1]._range
+
+            for (let i = 0; i < newVChildren.length; i++) {
+                const newVChild = newVChildren[i];
+                const oldVChild = oldVChildren[i];
+                if(i<oldVChildren.length){
+                    update(oldVChild,newVChild)
+                }else{
+                    //need insert newchild
+                    let range = document.createRange()
+                    range.setStart(tailRange.endContainer,tailRange.endOffset)
+                    range.setEnd(tailRange.endContainer,tailRange.endOffset)
+                    newVChild[RENDER_TO_DOM](range)
+                    tailRange = range
+                }
+                
+            }
+        }
+        let vdom = this.vdom
+        update(this._vdom,vdom)
+        this._vdom = vdom
     }
 
     setState(newState){//模拟react setState：合并新旧state，自动触发rerender
         //首先会假设已经有了state方法了，当然有可能是null，因此需要递归的形式去访问每一个的对象和属性：深度拷贝
         if(this.state === null || (typeof this.state !== 'object')){
             this.state = newState
-            this.rerender()
+            this.update()
             return
         }
         let merge = (oldState,newState)=>{
@@ -110,10 +108,81 @@ export class Component{
             }
         }
         merge(this.state,newState)
-        this.rerender()
+        this.update()
     }
 
 }
+
+
+class ElementWrapper extends Component{ //实体dom元素：得有root，可以挂载dom children或者挂载在别的实体dom上
+    constructor(type){
+        super(type)
+       // this.root = document.createElement(type)
+        this.type = type
+    }
+
+    get vdom(){
+        this.vchildren = this.children.map(child=>child.vdom)
+        return this
+    }
+
+
+    [RENDER_TO_DOM](range){
+        this._range = range
+        let root = document.createElement(this.type)
+        for (const name in this.props) {
+            const value = this.props[name]
+            if(name.match(/^on([\S\s]+)$/)){//tips: [\s\S]+ 表示匹配所有字符
+                root.addEventListener(RegExp.$1.replace(/^[\s\S]/,c=>c.toLowerCase()),value)//支持驼峰表达的事件
+            }else{
+                if(name === 'className'){//react把classname直接换成了class
+                    root.setAttribute('class',value)
+                }else{
+                    root.setAttribute(name,value)
+                }
+                
+            }
+        }
+
+        if(!this.vchildren){
+            this.vchildren = this.children.map(child=>child.vdom)
+        }
+        for (const child of this.vchildren) {
+            let childRange = document.createRange()
+            childRange.setStart(root,root.childNodes.length)
+            childRange.setEnd(root,root.childNodes.length)
+           
+            //2.把component的真实元素（root）放进去
+            child[RENDER_TO_DOM](childRange)
+        }
+        replaceContent(range,root) 
+
+    }
+}
+
+class TextWrapper extends Component{//实体dom text元素：得有root，可以被挂载在实体dom元素上。没有attr也没有child
+    constructor(content){
+        super(content)
+        this.type ='#text'
+        this.content = content
+    }
+
+    get vdom(){
+        return this
+        return {
+            type:'#text',
+            content:this.content
+        }
+    }
+    [RENDER_TO_DOM](range){
+        this._range = range
+        const root = document.createTextNode(this.content)
+        replaceContent(range,root)
+    }
+}
+
+//基于jsx语法的自定义组件，不是实体dom元素，而是由实体dom元素组成的虚拟dom元素类，必须有render方法，或者它的组成成分
+//在render函数返回的实体dom元素（可能包含别的虚拟dom元素类），
 
 export function createElement(elType,attributes,...children) {
     let element
@@ -148,6 +217,16 @@ export function createElement(elType,attributes,...children) {
     }
     insertChildren(children)
     return element
+}
+
+function replaceContent(range,node){
+    //insert
+    range.insertNode(node)
+    range.setStartAfter(node)
+    range.deleteContents()
+
+    range.setStartBefore(node)
+    range.setEndAfter(node)
 }
 
 //把虚拟元素实例挂载在实体dom上
